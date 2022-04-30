9a57f3a90bef7cd6a0f07a72499e1a441e7bee18
Luís Gustavo <luisgsilva270@gmail.com>
2022-04-30 14:54:24 +0000

    package and unittest

---
+++ section3/tests/unit/post_test.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from section3.post import Post
+
+
+class PostTest(TestCase):
+    
+    def test_create_post(self):
+        p = Post('Test', 'Test Content')
+
+        self.assertEqual(p.title, 'Test')
+        self.assertEqual(p.content, 'Test Content')
