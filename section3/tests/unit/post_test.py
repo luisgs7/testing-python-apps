506dffe58717fbb58ae76a43fa5e118d9c14d926
Luís Gustavo <luisgsilva270@gmail.com>
2022-04-30 15:23:03 +0000

    refactor post_test.py

--- section3/tests/unit/post_test.py
+++ section3/tests/unit/post_test.py
@@ -3,9 +3,8 @@ from section3.post import Post
 
 
 class PostTest(TestCase):
-    
     def test_create_post(self):
-        p = Post('Test', 'Test Content')
+        p = Post("Test", "Test Content")
 
-        self.assertEqual(p.title, 'Test')
-        self.assertEqual(p.content, 'Test Content')
+        self.assertEqual(p.title, "Test")
+        self.assertEqual(p.content, "Test Content")
