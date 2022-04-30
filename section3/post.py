08c8e859be24e559bd6e1f158185bbb6c6f4d601
Luís Gustavo <luisgsilva270@gmail.com>
2022-04-30 19:22:21 +0000

    new method json

--- section3/post.py
+++ section3/post.py
@@ -2,3 +2,9 @@ class Post:
     def __init__(self, title, content):
         self.title = title
         self.content = content
+
+    def json(self):
+        return {
+            "Title": self.title,
+            "Content": self.content,
+        }
