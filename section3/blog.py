8e796e48cfdc631565569f7a65ed374aceec2f47
luisgs7 <luisgsilva270@gmail.com>
2022-05-02 10:57:13 -0300

    refactor: implemented new methods create_post and json

--- section3/blog.py
+++ section3/blog.py
@@ -1,3 +1,6 @@
+from section3.post import Post
+
+
 class Blog:
     def __init__(self, title, author):
         self.title = title
@@ -5,15 +8,19 @@ class Blog:
         self.posts = []
 
     def __repr__(self):
-        return '{} by {} ({} post{})'.format(
+        return "{} by {} ({} post{})".format(
             self.title,
             self.author,
             len(self.posts),
-            's' if len(self.posts) != 1 else ''
+            "s" if len(self.posts) != 1 else "",
         )
 
     def create_post(self, title, content):
-        pass
+        self.posts.append(Post(title, content))
 
     def json(self):
-        pass
+        return {
+            "title": self.title,
+            "author": self.author,
+            "posts": [post.json() for post in self.posts],
+        }
