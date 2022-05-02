11fa88185a99ec228d1f174f9d8f23996119dcc2
luisgs7 <luisgsilva270@gmail.com>
2022-05-02 10:17:49 -0300

    refactor: new tests test_repr and test_repr_multiple_posts

--- section3/tests/unit/blog_test.py
+++ section3/tests/unit/blog_test.py
@@ -9,3 +9,19 @@ class BlogTest(TestCase):
         self.assertEqual("Test", b.title)
         self.assertEqual("Test Author", b.author)
         self.assertListEqual([], b.posts)
+
+    def test_repr(self):
+        b = Blog('Test', 'Test Author')
+        c = Blog('My Day', 'Rolf')
+
+        self.assertEqual(b.__repr__(), 'Test by Test Author (0 posts)')
+        self.assertEqual(c.__repr__(), 'My Day by Rolf (0 posts)')
+    
+    def test_repr_multiple_posts(self):
+        b = Blog('Test', 'Test Author')
+        b.posts = ['test']
+        c = Blog('My Day', 'Rolf')
+        c.posts = ['test', 'another']
+
+        self.assertEqual(b.__repr__(), 'Test by Test Author (1 post)')
+        self.assertEqual(c.__repr__(), 'My Day by Rolf (2 posts)')
